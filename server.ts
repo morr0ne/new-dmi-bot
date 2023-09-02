82a56b6ed68ec13d11000c26d0a06c513d694421
Federico Maria Morrone <contact@morrone.dev>
2023-09-02 15:22:05 +0200

    Added router to avoid crashes on get requests

--- server.ts
+++ server.ts
@@ -1,9 +1,21 @@
 import { bot } from "./bot.ts";
-import { Application } from "oak";
+import { Application, Router } from "oak";
 import { webhookCallback } from "grammy";
+
+const DOMAIN = "https://unict.morrone.dev";
+const API_ENDPOINT = "/telegram-bot-api-endpoints";
+
+bot.api.setWebhook(DOMAIN + API_ENDPOINT);
+
+const router = new Router();
+
+router
+  .get("/", (ctx) => {
+    ctx.response.body = "Questo non è il bot che stai cercando";
+  })
+  .use(API_ENDPOINT, webhookCallback(bot, "oak"));
 
 const app = new Application();
 
-app.use(webhookCallback(bot, "oak"));
-
-app.listen();
+app.use(router.routes());
+app.listen({ port: 8080 });
